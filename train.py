0cd4f9e0044c560b329f0047446b8e54012a08c4
Walter Wu <walterwuyan@gmail.com>
2024-03-06 14:03:36 +0000

    Modify default eval iters

--- train.py
+++ train.py
@@ -557,8 +557,8 @@ if __name__ == "__main__":
     parser.add_argument('--port', type=int, default=6009)
     parser.add_argument('--detect_anomaly', action='store_true', default=False)
     parser.add_argument("--test_iterations", nargs="+", type=int,
-                        default=[500, 1000, 2000, 5000, 7000] + list(range(10000, 40001, 2000)))
-    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 10_000, 20_000, 30_000, 40000])
+                        default=[500, 1000, 2000, 5000, 7000] + list(range(10000, 40001, 5000)))
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 20000, 40000])
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument('--log_im_every', type=int, default=99999999)
     args = parser.parse_args(sys.argv[1:])
